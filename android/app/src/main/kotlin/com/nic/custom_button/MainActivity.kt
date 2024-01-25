c1522e64bfe52c0b2ddf33e050ee557c1735dcaf
Iftikhar32 <ia988626@gmail.com>
2024-01-25 12:38:50 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/nic/custom_button/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nic.custom_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Button Example'),
+        ),
+        body: Center(
+          child: CustomButton(
+            color: Colors.blue, // Color for the button
+            containerColor: Colors.yellow, // Color for the container
+            onTap: () {
+              // Handle button tap
+              print('Button tapped!');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final Color color;
+  final Color containerColor;
+  final VoidCallback onTap;
+
+  CustomButton({required this.color, required this.containerColor, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 200,
+        height: 50,
+        alignment: Alignment.center, // Center the child (title text)
+        decoration: BoxDecoration(
+          color: containerColor,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black), // Border for the container
+        ),
+        child: Text(
+          'Custom Button',
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
